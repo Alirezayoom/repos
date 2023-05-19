7ddb449b36c3b39daf06a52a9564ef29c8e3fb1f
alirezayoom <alirezayoom@gmail.com>
2023-05-19 14:53:16 +0430

    update style

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,22 +9,20 @@ const pages = [
 
 const Header = () => {
   return (
-    <header className=" bg-[#222] py-8 px-4">
-      <div className="max-w-[64rem] mx-auto">
-        <div className="mb-4 mx-auto w-max">
-          <Link href="/">
-            <Image src="/ak.svg" width="50" height="50" />
-          </Link>
-        </div>
-        <div className="grid justify-center">
-          <ul className="flex gap-4">
-            {pages.map((page) => (
-              <li key={page.id}>
-                <Link href={page.link}>{page.name}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <header className=" bg-[#222] py-8 px-4 mx-auto">
+      <div className="mb-4 mx-auto w-max">
+        <Link href="/">
+          <Image src="/ak.svg" width="50" height="50" />
+        </Link>
+      </div>
+      <div className="grid justify-center">
+        <ul className="flex gap-4">
+          {pages.map((page) => (
+            <li key={page.id}>
+              <Link href={page.link}>{page.name}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </header>
   );
